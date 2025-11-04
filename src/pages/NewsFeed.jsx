c34c08917f39f1e25b3969b6701f68821a4f690f
rnvelascoua <robert.velasco@ua.edu.ph>
2025-11-04 14:07:19 +0800

    updated API URL again

--- src/pages/NewsFeed.jsx
+++ src/pages/NewsFeed.jsx
@@ -4,7 +4,7 @@ import PostCard from "../components/PostCard";
 import PostForm from "../components/PostForm";
 import { Box, Container, Typography, CircularProgress } from "@mui/material";
 
-const API_URL = "https://post-api-mahp.onrender.com/api/posts"; // Update with your API URL
+const API_URL = "https://facebook-api-e5s9.onrender.com/api/posts"; // Update with your API URL
 
 const NewsFeed = () => {
   const [posts, setPosts] = useState([]);
